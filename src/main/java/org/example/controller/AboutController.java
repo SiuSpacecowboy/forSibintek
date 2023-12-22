178f79a49008851e48d37ca56159d530b3bb95b7
SiUSpacecowboy <danvitaly14@mail.ru>
2023-12-22 05:41:47 +0300

    Final version

--- src/main/java/org/example/controller/AboutController.java
+++ src/main/java/org/example/controller/AboutController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AboutController {
 
     @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
     public String about() {
         return "about";
     }
